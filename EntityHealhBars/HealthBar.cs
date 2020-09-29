47c2ae15c1177a70c15d141ef77f0595832a2fed
slesty <53488160+slesty@users.noreply.github.com>
2020-09-29 17:46:39 -0400

    Add files via upload

---
+++ EntityHealhBars/HealthBar.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public GameObject healthBarUI;
+    public Slider slider;
+    public Entity owner;
+
+    void Update()
+    {
+
+        Player player = Player.localPlayer;
+        if (player != null)
+
+        {
+            //Health
+            slider.value = owner.HealthPercent();
+            if (slider.value == 1)
+            {
+                healthBarUI.SetActive(true);
+            }
+            if (slider.value == 0)
+            {
+                healthBarUI.SetActive(false);
+            }
+                
+        }
+
+    }
+}
+
+
+
+
+            
+
+        
+   
+
